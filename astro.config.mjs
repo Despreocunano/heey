0e2d57aab2c8da4f77cd7cb552629c3f6620beb9
Despreocunano <hola@sorostica.com>
2024-09-23 13:38:33 -0300

    push initial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
-import vercel from '@astrojs/vercel/serverless';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel()
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,1 @@
 /// <reference path="../.astro/types.d.ts" />
-interface ImportMetaEnv {
-    readonly STRAPI_URL: string;
-  }
