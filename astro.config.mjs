52b8e86c018170901cf0266b4d042da7642f90bd
Despreocunano <hola@sorostica.com>
2025-01-08 16:13:31 -0300

    sad

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',  // Cambiado a 'server' para permitir renderizado en el servidor
+
+  // Cambiado a 'server' para permitir renderizado en el servidor
+  output: 'server',
+
+  adapter: vercel()
 });
