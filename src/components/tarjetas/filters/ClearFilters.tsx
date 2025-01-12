6d59117ab15cd1aad6cad2cd4de9a1a762d62d39
Despreocunano <hola@sorostica.com>
2025-01-12 00:05:33 -0300

    adjust color

--- src/components/tarjetas/filters/ClearFilters.tsx
+++ src/components/tarjetas/filters/ClearFilters.tsx
@@ -9,7 +9,7 @@ export function ClearFilters() {
   return (
     <button
       onClick={handleClearFilters}
-      className="inline-flex items-center gap-2 px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-lg hover:bg-gray-50 hover:text-teal-600 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+      className="inline-flex items-center gap-2 px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-lg hover:bg-gray-50 hover:text-teal-500 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
     >
       <svg 
         xmlns="http://www.w3.org/2000/svg" 
